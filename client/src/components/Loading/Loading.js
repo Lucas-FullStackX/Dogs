fb6b729cc2e738092960c3e786789693b3c5fc5e
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2021-07-30 13:11:19 -0500

    deploy api

--- client/src/components/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Loading.module.css";
-
-const Loading = () => {
-  return <div className={styles.lds_dual_ring}></div>;
-};
-
-export default Loading;
--- client/src/const.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "http://localhost:3001/dogs";
-const TEMP_URL = "http://localhost:3001/temperament";
-const POST_DOG_URL = "http://localhost:3001/dog";
-
-module.exports = {
-  BASE_URL,
-  TEMP_URL,
-  POST_DOG_URL,
-};
