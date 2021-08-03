6dac4513f9f24f0b30d02323c62c715afc15469d
Lucas Leguizamo <78978776+Lucas-FullStackX@users.noreply.github.com>
2021-08-03 18:53:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import Menu from "./components/Menu/Menu";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
 import Form from "./components/Form/Form";
 import Dog from "./components/Dog/Dog";
+const URL = process.env.PUBLIC_URL;
+
 function App() {
   return (
     <div className="App">
-      <Route path="/home" component={Nav}></Route>
+      <Route path={URL + "/home"} component={Nav}></Route>
       <Route exact path="/home" component={Menu}></Route>
       <Switch>
-        <Route exact path="/" component={Start}></Route>
-        <Route exact path="/home" component={Home}></Route>
-        <Route path="/home/newdog" component={Form}></Route>
-        <Route exact path="/home/:id" component={Dog}></Route>
+        <Route exact path={URL + "/"} component={Start}></Route>
+        <Route exact path={URL + "/home"} component={Home}></Route>
+        <Route path={URL + "/home/newdog"} component={Form}></Route>
+        <Route exact path={URL + "/home/:id"} component={Dog}></Route>
         <Route path="*" component={NotFoundPage}></Route>
       </Switch>
     </div>
